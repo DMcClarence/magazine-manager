28270429d5f54e42029959414fa86cb856211cef
Dylan McClarence <34085068@student.murdoch.edu.au>
2025-04-28 16:57:09 +0800

    Create Netbeans Project

---
+++ MagazineService/src/magazineservice/MagazineService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package magazineservice;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 34085068
+ */
+public class MagazineService extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
